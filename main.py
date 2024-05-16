1f3a1b0a2d2d7edba28d0d0bad37b9243abecef5
kylegit <amerikefang@gmail.com>
2024-05-15 18:13:48 -0700

    Initial commit

--- main.py
+++ main.py
@@ -45,8 +45,8 @@ class Base(DeclarativeBase):
     pass
 
 #'sqlite:///posts.db'
-app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('DB_URI','sqlite:///posts.db')
-#app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///posts.db'
+#app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('DB_URI','sqlite:///posts.db')
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///posts.db'
 db = SQLAlchemy(model_class=Base)
 db.init_app(app)
 
